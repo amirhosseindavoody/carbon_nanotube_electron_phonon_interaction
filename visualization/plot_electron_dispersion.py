Phonon dispersion calculation is fixed.
Now, I am ready to find the correct momentum and energy conserving phonon modes for calculating electron-phonon scattering rates.

visualization/plot_electron_dispersion.py
@@ -1,79 +1,58 @@
 # this python program reads the calculated phonon dispersion of carbon nanotubes and plots the dispersion curves.
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import itertools
 
+# prepare for ploting
+figure_list = list() # this will hold the list of all the figures
+plt.ion() # enables interactive mode for ploting. No plt.show() is needed!
+colors = itertools.cycle(["r", "b", "g", "k", "c", "m", "y"]) # iterative list for cycling through colors when ploting. use color=next(colors) to get the next color on the list.
+styles = itertools.cycle(["solid", "dashed", "dash_dot"]) # iterative list for cycling through line styles when ploting. use linestyle=next(styles) to get the next color on the list.
+
+# set some constants
 eV = 1.6e-19 #[Jouls]
 eV_to_inverse_cm = 8065.54429
 hbar = 1.054e-34 #[Jouls.second]
 vF = 1.0e6
 
-data = np.loadtxt("/home/amirhossein/research/exciton/data/transfer_rates/tmp_001/cnt1.electron_k_vector.dat", skiprows=0)
-k_vec = np.array(data)
+
+directory = "/home/amirhossein/research/exciton/data/transfer_rates/tmp_001/"
+cnt_name = "cnt1"
+
+################################################################################
+
+k_vec = np.loadtxt(directory+cnt_name+".electron_k_vector.dat", skiprows=0)
 
 data = np.loadtxt("/home/amirhossein/research/exciton/data/transfer_rates/tmp_001/cnt1.electron_conduction_band.dat", skiprows=0)
 Ec = np.array(np.transpose(data[:,:]))
 
-data = np.loadtxt("/home/amirhossein/research/exciton/data/transfer_rates/tmp_001/cnt1.electron_valence_band.dat", skiprows=0)
-Ev = np.array(np.transpose(data[:,:]))
-
-# convert the units to electron-volt
+filename = directory+cnt_name+".electron_conduction_band.dat"
+Ec = np.loadtxt(filename, skiprows=0)
+Ec = Ec.T
 Ec = Ec/eV
+
+filename = directory+cnt_name+".electron_valence_band.dat"
+Ev = np.loadtxt(filename, skiprows=0)
+Ev = Ev.T
 Ev = Ev/eV
 
-data = np.loadtxt("/home/amirhossein/research/exciton/data/transfer_rates/tmp_001/phonon_dispersion.dat", skiprows=0)
+fig = plt.figure()
+figure_list.append(fig)
+axes = fig.add_subplot(111)
 
-q_vec = np.array(data[0,:])
-omega = np.array(np.transpose(data[1:-1,:]))
+for i in range(0,Ec.shape[1]):
+	axes.plot(k_vec,Ec[:,i], linewidth=2.0, linestyle="solid")
+	axes.plot(k_vec,Ev[:,i], linewidth=2.0, linestyle="solid")
 
-# convert the units to electron-volt
-omega = omega/eV
+xmin = min(k_vec)
+xmax = max(k_vec)
+axes.set_xlim([xmin,xmax])
 
+ymin = 1.01*np.amin(Ev)
+ymax = 1.01*np.amax(Ec)
+axes.set_ylim([ymin,ymax])
 
+fig.canvas.draw()
 
-# convert omega units to inverse cm.
-# omega = omega*eV_to_inverse_cm
-
-# convert k_vector units
-# hbar*vF/eV*k_vec
-
-plt.plot(k_vec[:],Ec[:,:],'b-')
-plt.plot(k_vec[:],Ev[:,:],'r-')
-plt.plot(q_vec[:],omega[:,:],'k-')
-# plt.tight_layout()
-plt.show()
-
-# k_vec = data[0,:]
-# omega = data[]
-# plt.plot(data_dw[:,0], data_dw[:,1], 'r-')
-#
-# data_up = np.loadtxt("ld1.wfc.up", skiprows=1)
-# plt.plot(data_up[:,0], data_up[:,1], 'b--')
-#
-# xmin = 0
-# xmax = 10
-# ymin = -0.7
-# ymax = 0.3
-# axes = plt.gca()
-# axes.set_xlim([xmin,xmax])
-# axes.set_ylim([ymin,ymax])
-#
-# plt.show()
-
-# array = np.zeros((2,3),np.float)
-# print "array = \n", array
-#
-# print "range = ", range(0,2)
-#
-# k=0
-# for i in range(0,2):
-# 	for j in range(0,3):
-# 		k = k+1
-# 		array[i,j] = k
-#
-# print "array = \n", array
-#
-# for row in range(0,shp[0]-2):
-# 	print row
-# 	plt.plot(k_vec[:],omega[row,:],'r-')
-#
-# plt.show()
+raw_input("Press Enter to exit...")

visualization/plot_phonon_dispersion.py
@@ -17,15 +17,17 @@ hbar = 1.054e-34 #[Jouls.second]
 vF = 1.0e6
 
 
-directory = "/home/amirhossein/research/exciton/data/transfer_rates/tmp_005/"
+directory = "/home/amirhossein/research/exciton/data/transfer_rates/tmp_001/"
 cnt_name = "cnt1"
 
 ################################################################################
+
 k_vec = np.loadtxt(directory+cnt_name+".phonon_k_vector.dat", skiprows=0)
 
 filename = directory+cnt_name+".phonon_energy.dat"
 phonon_energy = np.loadtxt(filename, skiprows=0)
 phonon_energy = phonon_energy.T
+phonon_energy = phonon_energy/eV
 
 fig = plt.figure()
 figure_list.append(fig)
@@ -38,10 +40,12 @@ xmin = min(k_vec)
 xmax = max(k_vec)
 axes.set_xlim([xmin,xmax])
 
-ymin = np.amin(phonon_energy)
-ymax = np.amax(phonon_energy)
+ymin = 1.1*np.amin(phonon_energy)
+ymax = 1.1*np.amax(phonon_energy)
 axes.set_ylim([ymin,ymax])
 
 fig.canvas.draw()
 
-raw_input("Press Enter to continue...")
+# raw_input("Press Enter to continue...")
+
+raw_input("Press Enter to exit...")